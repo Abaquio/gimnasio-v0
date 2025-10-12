64211fa1a2c8f2d61fd490346ebd8f9d51b56b03
Abaquio <matt.sepulveda00@gmail.com>
2025-10-12 16:21:20 -0300

    build: demo en docs para GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,10 @@
-// vite.config.js
 import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
 
-export default defineConfig({
-  base: "/gimnasio-v0/",  // 👈 usa el nombre exacto del repo en GitHub
+export default defineConfig(({ mode }) => ({
+  base: mode === "production" ? "/gimnasio-v0/" : "/",  // 👈 dinámico
   plugins: [react()],
   build: {
-    outDir: "dist",       // carpeta temporal de build
+    outDir: "docs",   // para GitHub Pages
   },
-})
+}))
