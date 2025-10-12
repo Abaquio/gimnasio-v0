4e2592754d52d3f05523c5136909c4140043deaa
Abaquio <matt.sepulveda00@gmail.com>
2025-10-12 15:56:35 -0300

    gh-pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.js
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react"
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: "/gimnasio-v0/",  // 👈 usa el nombre exacto del repo en GitHub
   plugins: [react()],
+  build: {
+    outDir: "dist",       // carpeta temporal de build
+  },
 })
